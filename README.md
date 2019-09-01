99f7f03a838f91fc219e823a14da9653c13b1b44
Alexander Meißner <AlexanderMeissner@gmx.net>
2019-09-01 21:22:28 +0200

    Renamed pypi package name to be lower case

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PyPiler
 DAG based data-flow and control-flow representation for meta programming
+
+```pip3 install pypiler```
--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(
-    name = 'PyPiler',
+    name = 'pypiler',
     version = '0.0.1',
     license = 'MIT',
     description = 'DAG based data-flow and control-flow representation for meta programming',
     python_requires = '>=3.3',
     author = 'Alexander Meißner',
     author_email = 'AlexanderMeissner@gmx.net',
-    url = 'https://github.com/Symatem/PiPiler',
-    packages = ['PyPiler'],
+    url = 'https://github.com/Symatem/PyPiler',
+    packages = ['pypiler'],
     install_requires = ['tree_sitter >= 0.0.8']
 )
